9d91003aca4efe36c2d50044e48f2e4965db6932
Franklin Shera <fshera96@gmail.com>
2023-04-06 12:09:15 +0300

    works

--- packages/ui/src/components/Button.tsx
+++ packages/ui/src/components/Button.tsx
@@ -1,5 +1,18 @@
 import React, { FC } from "react";
 
 export const Button: FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <button className="ui-button">{children}</button>;
+  return (
+    <button
+      onClick={() => alert("UI Button Clicked!")}
+      className="ui-button"
+      style={{
+        padding: "20px 50px",
+        background: "#111",
+        border: "none",
+        cursor: "pointer",
+      }}
+    >
+      {children}
+    </button>
+  );
 };
